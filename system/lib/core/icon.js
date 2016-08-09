469302aea7985e8aee17914eb5e3893abdcbc6f4
Nana Axel <ax.lnana@outlook.com>
2016-08-09 06:39:45 +0200

    Use the application's contextual menu cache

--- system/lib/core/icon.js
+++ system/lib/core/icon.js
@@ -1,7 +1,5 @@
 +function ($) {
 
-    var ext_ctx_menu = {};
-
     FlatOS.UI.Icon = function(opt) {
         if (typeof opt === 'string') {
             opt = {path: opt};
@@ -49,62 +47,68 @@
         },
 
         getContextMenu: function() {
-            var that = this;
-            var regApps = this._a.getRegApps(this.type),
-                contextMenu = {}, command = [];
-            var Callback = new FlatOS.Callback();
-            for (var i = 0, l = regApps.length; i < l; i++) {
-                var app_name = regApps[i];
-                var _app = new FlatOS.Application(app_name);
-                var config = _app.getAppConfig();
-                if (typeof config.contextMenu !== 'undefined') {
-                    var info   = _app.getInfo();
-                    var titleMenu = {};
-                    titleMenu[app_name] = {
-                        title: {
-                            name: info.app_name,
-                            icon: info.app_icon,
-                            title: true
-                        }
-                    };
-                    for (var menu in config.contextMenu[app_name]) {
-                        command.push(config.contextMenu[app_name][menu]);
-                    }
-                    $(command).each(function() {
-                        var command = this.action;
-                        this.callback = function() {
-                            _app.launch({
-                                callback: function() {
-                                    Callback.call('contextMenu', app_name+'_'+command, that.opt.path);
-                                }
-                            });
+            if (!F.Cache.ContextMenu.hasOwnProperty(this.opt.path)) {
+                var that = this;
+                var regApps = this._a.getRegApps(this.type),
+                    contextMenu = {}, command = [];
+                var Callback = new FlatOS.Callback();
+                for (var i = 0, l = regApps.length; i < l; i++) {
+                    var app_name = regApps[i];
+                    var _app = new FlatOS.Application(app_name);
+                    var config = _app.getAppConfig();
+                    if (typeof config.contextMenu !== 'undefined') {
+                        var info = _app.getInfo();
+                        var titleMenu = {};
+                        titleMenu[app_name] = {
+                            title: {
+                                name: info.app_name,
+                                icon: info.app_icon,
+                                title: true
+                            }
                         };
-                    });
-                    ctm = $.extend(
-                        {},
-                        titleMenu[app_name],
-                        config.contextMenu[app_name]
-                    );
-                    contextMenu[app_name] = $.extend(
-                        {},
-                        ctm
-                    );
+                        for (var menu in config.contextMenu[app_name]) {
+                            command.push(config.contextMenu[app_name][menu]);
+                        }
+                        $(command).each(function () {
+                            var command = this.action;
+                            this.callback = (function (_a, _n, _c, _t) {
+                                return function () {
+                                    _a.launch({
+                                        callback: function () {
+                                            Callback.call('contextMenu', _n + '_' + _c, _t.opt.path);
+                                        }
+                                    });
+                                };
+                            })(_app, app_name, command, that);
+                        });
+                        var ctm = $.extend(
+                            {},
+                            titleMenu[app_name],
+                            config.contextMenu[app_name]
+                        );
+                        contextMenu[app_name] = $.extend(
+                            {},
+                            ctm
+                        );
+                    }
                 }
-            }
-            ext_ctx_menu[this.opt.path] = $.extend(
-                {
-                    open: {
+                F.Cache.ContextMenu[this.opt.path] = $.extend(
+                    {
                         open: {
-                            name: 'Open',
-                            callback: function() {
-                                that._a.openWithDefault(that.opt.path);
+                            open: {
+                                name: 'Open',
+                                callback: (function (that) {
+                                    return function () {
+                                        that._a.openWithDefault(that.opt.path);
+                                    };
+                                })(this)
                             }
                         }
-                    }
-                },
-                contextMenu
-            );
-            return ext_ctx_menu[this.opt.path];
+                    },
+                    contextMenu
+                );
+            }
+            return F.Cache.ContextMenu[this.opt.path];
         },
 
         _initContextMenu: function() {
@@ -115,10 +119,11 @@
         },
 
         _initDoubleClick: function() {
-            var that = this;
-            this._m.doubleClick('application.icon.launch', this.icon, function(e) {
-                that._a.openWithDefault(that.opt.path);
-            });
+            this._m.doubleClick('application.icon.launch', this.icon, (function(that) {
+                return function () {
+                    that._a.openWithDefault(that.opt.path);
+                };
+            })(this));
         }
 
     };
